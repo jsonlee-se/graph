ca9362b51557f1279379052d51f61316e0967d1b
Jayson-Lee Gillis <jayson-lee.gillis@hotmail.be>
2026-04-27 18:58:59 +0200

    chore(project): cleanup of prev branding & demo data

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 // Configuration options
-const init_phones = ["Haruto 2024 Target", "AudioSense DT200"],// Optional. Which graphs to display on initial load. Note: Share URLs will override this set
+const init_phones = ["Universal ∆", "AudioSense DT200"],// Optional. Which graphs to display on initial load. Note: Share URLs will override this set
       DIR = "data/",                                // Directory where graph files are stored
       default_channels = ["L","R"],                 // Which channels to display. Avoid javascript errors if loading just one channel per phone
       default_normalization = "dB",                 // Sets default graph normalization mode. Accepts "dB" or "Hz"
@@ -11,14 +11,14 @@ const init_phones = ["Haruto 2024 Target", "AudioSense DT200"],// Optional. Whic
       alt_animated = false,                         // Determines if new graphs are drawn with a 1-second animation, or appear instantly
       alt_header = true,                            // Display a configurable header at the top of the alt layout
       alt_header_new_tab = false,                   // Clicking alt_header links opens in new tab
-      alt_tutorial = true,                          // Display a configurable frequency response guide below the graph
+      alt_tutorial = false,                          // Display a configurable frequency response guide below the graph
       alt_augment = true,                           // Display augment card in phone list, e.g. review sore, shop link
       site_url = '/',                               // URL of your graph "homepage"
       share_url = true,                             // If true, enables shareable URLs
-      watermark_text = "CrinGraph",                 // Optional. Watermark appears behind graphs
-      watermark_image_url = "assets/images/haruto.svg", // Optional. If image file is in same directory as config, can be just the filename
-      rig_description = "clone IEC 711",            // Optional. Labels the graph with a description of the rig used to make the measurement, e.g. "clone IEC 711"
-      page_title = "CrinGraph",                     // Optional. Appended to the page title if share URLs are enabled
+      watermark_text = "",                 // Optional. Watermark appears behind graphs
+      watermark_image_url = "", // Optional. If image file is in same directory as config, can be just the filename
+      rig_description = "clone IEC 60318-4",            // Optional. Labels the graph with a description of the rig used to make the measurement, e.g. "clone IEC 711"
+      page_title = "JSON",                     // Optional. Appended to the page title if share URLs are enabled
       page_description = "View and compare frequency response graphs for earphones",
       accessories = true,                           // If true, displays specified HTML at the bottom of the page. Configure further below
       externalLinksBar = true,                      // If true, displays row of pill-shaped links at the bottom of the page. Configure further below
@@ -40,10 +40,9 @@ const init_phones = ["Haruto 2024 Target", "AudioSense DT200"],// Optional. Whic
 
 // Specify which targets to display
 const targets = [
-    { type:"Reference",  files:["Haruto 2024","Haruto 2021"] },
+    { type:"∆",  files:["Universal ∆"] },
     { type:"Neutral",    files:["KEMAR DF","IEF Neutral 2023","Etymotic"] },
-    { type:"Reviewer",   files:["Antdroid","Banbeucmas","HBB","Precogvision","Super Review 22","Timmy","VSG"] },
-    { type:"Preference", files:["Harman IE 2019v2","Harman IE 2017v2","AutoEQ","Rtings","Sonarworks"] }
+    { type:"Preference", files:["Harman IE 2019v2","Harman IE 2017v2","Rtings","Sonarworks"] }
 ];
 
 // Haruto's Addons
@@ -55,12 +54,12 @@ const  preference_bounds_name = "Bounds",              // Preference bounds name
        default_y_scale = "40db",                       // Default Y scale; values: ["20db", "30db", "40db", "50db", "crin"]
        default_DF_name = "KEMAR DF",                   // Default RAW DF name
        dfBaseline = true,                              // If true, DF is used as baseline when custom df tilt is on
-       default_bass_shelf = 8,                         // Default Custom DF bass shelf value
-       default_tilt = -0.8,                            // Default Custom DF tilt value
+       default_bass_shelf = 0,                         // Default Custom DF bass shelf value
+       default_tilt = -1,                            // Default Custom DF tilt value
        default_ear = 0,                                // Default Custom DF ear gain value
        default_treble = 0,                             // Default Custom DF treble gain value
-       tiltableTargets = ["KEMAR DF"],                 // Targets that are allowed to be tilted
-       compTargets = ["KEMAR DF"],                     // Targets that are allowed to be used for compensation
+       tiltableTargets = ["KEMAR DF", "Universal ∆"],                 // Targets that are allowed to be tilted
+       compTargets = ["KEMAR DF", "Universal ∆"],                     // Targets that are allowed to be used for compensation
        allowCreatorSupport = true;                     // Allow the creator to have a button top right to support them
 
 
@@ -102,16 +101,6 @@ function watermark(svg) {
             .attrs({x:380, y:-134, "font-size":8, "text-anchor":"end", "class":"rig-description", "style": "filter: var(--svg-filter);"})
             .text("Measured on: " + rig_description);
     }
-    
-    let wmSq = svg.append("g")
-        .attr("opacity",0.2);
-    
-    wmSq.append("image")
-        .attrs({x:652, y:254, width:100, height:94, "class":"wm-squiglink-logo", "xlink:href":"assets/images/squiglink-giggle.svg"});
-    
-    wmSq.append("text")
-        .attrs({x:641, y:314, "font-size":10, "transform":"translate(0,0)", "text-anchor":"end", "class":"wm-squiglink-address"})
-        .text("squig.link/lab/harutohiroki");
 }
 
 
@@ -248,21 +237,19 @@ setupGraphAnalytics();
 
 
 // If alt_header is enabled, these are the items added to the header
-let headerLogoText = "HarutoHiroki",
-    headerLogoImgUrl = "assets/images/haruto.svg",
+let headerLogoText = "json",
+    headerLogoImgUrl = "",
     headerLinks = [
     {
-        name: "Sample",
-        url: "https://sample.com"
-    },
-    {
-        name: "Sample External",
+        name: "Blog (TODO)",
         url: "https://sample.com",
         external: true
-    }
+    },
+
 ];
 
 // Source: https://www.teachmeaudio.com/mixing/techniques/audio-spectrum
+// this is not being used rn - json
 let tutorialDefinitions = [
     {
         name: 'Sub bass',
--- assets/js/graphAnalytics.js
+++ assets/js/graphAnalytics.js
@@ -1,5 +1,5 @@
 // Set these variables to your own GTM ID and site name
-let analyticsSite = "HarutoHiroki",                // Site name for attributing analytics events to your site
+let analyticsSite = "JSON",                // Site name for attributing analytics events to your site
     analyticsGtmId = "",                 // GTM ID used for analytics. If you don't already have one, you'' need to create a Google Tag Manager account
     logAnalytics = true;                            // If true, events are logged in console
 
